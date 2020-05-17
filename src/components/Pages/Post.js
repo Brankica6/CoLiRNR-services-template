f4b0891de4bee05c390c7623211322ae0a72047d
Brankica <brankicadjordjevic0@gmail.com>
2020-05-17 16:35:30 +0200

    little changed style and added a "Recent Posts"

--- src/components/Pages/Post.js
+++ src/components/Pages/Post.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 import Footer from '../Common/Footer.js';
 import Jumbotron from '../Common/Jumbotron.js';
 import img1 from '../../assets/images/bg1.jpg';
@@ -10,51 +10,58 @@ class Post extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
-      imgUrl:'',
-      description: '',
-      date:''
+      posts: [],
+      currentSlug: ''
     };
   }
   componentDidMount = () => {
-    const id = this.props.match.params.slug;
-    this.findPost(id);
+    const slug = this.props.match.params.slug;
+    const result = postsData.post.filter(post => {
+      return post.slug === slug;
+    })
+    return {
+      posts: result[0],
+      currentId: slug,
+    }
   }
-  findPost = (slug) => {
-    postsData.post.map((p) => {
-      if(p.slug === slug) {
-        return this.setState({
-          title: p.title,
-          imgUrl:p.imgUrl,
-          description:p.desc,
-          date: p.date
-        });
-      } else {
-        return false;
-      }
-     });
-    };
-    render(){
-      return (
+
+  static getDerivedStateFromProps = (nextProps, prevState) => {
+   if (nextProps.match.params.slug !== prevState.currentSlug){
+     const currentSlug = nextProps.match.params.slug
+     const result = postsData.post.filter(obj => {
+       return obj.slug === currentSlug;
+     })
+     return {
+      posts: result[0],
+      currentSlug: currentSlug,
+   }
+ }
+   return null;
+}
+
+
+  render(){
+    const {imgUrl, title, date, description} = this.state.posts;
+    return (
       <div>
         <Jumbotron image = {img1} title={this.state.title} description={''} page={'Post'}/>
         <section className="section-8">
-    		   <div className="section-wrapper">
+      	   <div className="section-wrapper">
              <div className="row">
                <div className="col-lg-8">
                  <div className="blog-details">
                     <div className="post-image mb-5">
-                        <img className="img-fluid" src={this.state.imgUrl} alt={this.state.title}/>
+                        <img className="img-fluid" src={imgUrl} alt={this.state.title}/>
                     </div>
-                    <h3>{this.state.title}</h3>
+                    <h3>{title}</h3>
                     <div className="blog-meta">
                         <ul>
                             <li><i className="fas fa-user"></i> <a href="/">Admin</a></li>
-                            <li><i className="far fa-clock"></i>{this.state.date} </li>
+                            <li><i className="far fa-clock"></i>{date} </li>
                             <li><i className="fas fa-folder-open"></i> <a href="/">Post</a></li>
                         </ul>
-    						    </div>
-      							<p>{this.state.description}</p>
+      					    </div>
+      							<p>{description}</p>
       							<blockquote className="blockquote">
       								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -68,16 +75,22 @@ class Post extends Component {
                </div>
                <div className="col-lg-4">
                  <div className="sidebar">
-                    <div className="widget widget_categories widget_links mt-5">
+                    <div className="widget widget_recent_entries mt-5">
         								<h3 className="widget-title">
-        									Categories
+        									Recent Posts
         								</h3>
         								<ul>
-        									<li><a href="/">Item1</a></li>
-        									<li><a href="/">Item2</a></li>
-        									<li><a href="/">Item3</a></li>
-        									<li><a href="/">Item4</a></li>
-        									<li><a href="/">Item5</a></li>
+                        { postsData.post.slice(0,4).map((p,index) => {
+                          return <li key={index}>
+                										<Link className="img-link" to={`/post/${p.slug}`}>
+                											<img src= {p.imgUrl} alt={p.title}/>
+                										</Link>
+                                    <div>
+                  										<Link to={{pathname:`/post/${p.slug}`}}>{p.title}</Link>
+                  										<p className="date">{p.date}</p>
+                                    </div>
+      									            </li>
+                        })}
         								</ul>
                     </div>
 
@@ -86,21 +99,24 @@ class Post extends Component {
       									Tags
       								</h3>
       								<div className="tagcloud">
-      									<a href="/">Error</a>
-      									<a href="/">Cake Bake</a>
-      									<a href="/">Dromzone</a>
-      									<a href="/">File</a>
-      									<a href="/">Yii</a>
-      									<a href="/">Yii2</a>
+                        <ul>
+        									<li>Error</li>
+        									<li>Post</li>
+        									<li>Zip</li>
+        									<li>File</li>
+        									<li>Yii</li>
+        									<li>Yii2</li>
+                        </ul>
       								</div>
                     </div>
                   </div>
                </div>
              </div>
-    		 	</div>
-    		</section>
+      	 	</div>
+      	</section>
       <Footer/>
     </div>
   )}
 }
-export default withRouter(Post);
+
+export default Post;
