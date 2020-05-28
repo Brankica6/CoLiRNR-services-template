8ccaa64b39ddc884cf22e63c486355756f3c5a50
Brankica <brankicadjordjevic0@gmail.com>
2020-05-28 19:36:19 +0200

    Updated posts

--- src/Data/posts.js
+++ src/Data/posts.js
@@ -1,44 +1,58 @@
  export const post = [
   {
     imgUrl: require("../assets/images/blog/img1.jpg"),
-    date:'03.05.2020',
+    date:'22.05.2021',
     title: 'The Best Marketing top Management Tools',
     desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.',
     slug:'post1'
   },
   {
     imgUrl: require("../assets/images/blog/img2.jpg"),
-    date:'03.05.2020',
+    date:'03.05.2021',
     title: 'Cras mush pardon you knees up he lost his bottle it’s all gone to pot faff',
     desc: 'Bloke cracking goal the full monty get stuffed mate posh wellies fantastic knackered tickety-boo Harry porkies, mush excuse my French bender down the pub Oxford bum bag gutted mate car boot pukka loo it’s your round, cor blimey guvnor is on your bike mate cup of char some dodgy chav blag happy days nancy boy hotpot.',
     slug:'post2'
   },
   {
     imgUrl: require("../assets/images/blog/img3.jpg"),
-    date:'03.05.2020',
+    date:'03.05.2021',
     title: 'Aliquam mollit nemo taciti',
     desc: 'Officiis similique repellat parturient error earum conubia expedita, porta mollis diamlorem est autem feugiat nibh. Gravida voluptatem aute, non eget. Ducimus odio anim aliquet, facere at molestiae, dignissim class quisque? ',
     slug:'post3'
   },
   {
     imgUrl: require("../assets/images/blog/img4.jpg"),
-    date:'01.04.2020',
+    date:'20.04.2021',
     title: 'Interdum luctus accu samus',
     desc: 'Cras mush pardon you knees up he lost his bottle it’s all gone to pot faff about porkies arse, barney argy-bargy cracking goal loo cheers spend a penny bugger all mate in my flat, hunky-dory well get stuffed mate David morish bender lavatory.',
     slug:'post4'
   },
   {
     imgUrl: require("../assets/images/blog/img5.jpg"),
-    date:'03.04.2020',
+    date:'03.04.2021',
     title: 'Cancel my subscription',
     desc: 'Looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
     slug:'post5'
   },
   {
     imgUrl: require("../assets/images/blog/img6.jpg"),
-    date:'04.05.2020',
+    date:'02.04.2021',
     title: 'Resume notifications only when you’re ready',
     desc: 'Looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the. Contrary to popular belief, Lorem Ipsum is not simply random text. ',
     slug:'post6'
+  },
+  {
+    imgUrl: require("../assets/images/blog/img5.jpg"),
+    date:'02.02.2021',
+    title: 'Cancel my subscription',
+    desc: 'Looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    slug:'post7'
+  },
+  {
+    imgUrl: require("../assets/images/blog/img3.jpg"),
+    date:'01.05.2021',
+    title: 'Aliquam mollit nemo taciti',
+    desc: 'Officiis similique repellat parturient error earum conubia expedita, porta mollis diamlorem est autem feugiat nibh. Gravida voluptatem aute, non eget. Ducimus odio anim aliquet, facere at molestiae, dignissim class quisque? ',
+    slug:'post8'
   }
 ]
